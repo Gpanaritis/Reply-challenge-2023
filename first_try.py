381138666915659689ff134a535a34c242600e7d
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2023-03-09 18:14:35 +0200

    Directions. No wormholes

--- first_try.py
+++ first_try.py
@@ -1,3 +1,39 @@
+def down(i,j):
+    if i < rows-1:
+        if system[i+1][j] == "*":
+            return
+        else:
+            return i+1,j,int(system[i+1][j])
+    elif i == rows-1:
+        return 0,j,int(system[0][j])
+
+def up(i,j):
+    if i > 0:
+        if system[i-1][j] == "*":
+            return
+        else:
+            return i-1,j,int(system[i-1][j])
+    elif i == 0:
+        return rows-1,j,int(system[rows-1][j])
+
+def right(i,j):
+    if j < columns-1:
+        if system[i][j+1] == "*":
+            return
+        else:
+            return i,j+1,int(system[i][j+1])
+    elif j == columns-1:
+        return i,0,int(system[i][0])
+
+def left(i,j):
+    if j > 0:
+        if system[i][j-1] == "*":
+            return
+        else:
+            return i,j-1,int(system[i][j-1])
+    elif j == 0:
+        return i,columns-1,int(system[i][columns-1])
+
 f = open("00-example.txt", "r")
 
 line = f.readline()
